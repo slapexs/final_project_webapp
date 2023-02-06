22fa80a32b39b73123722e717b92c0cc55585321
slapexs <slapexs@gmail.com>
2023-02-06 20:43:35 +0700

    :lipstick: Add copyright into footer

--- components/Footer.js
+++ components/Footer.js
@@ -6,20 +6,33 @@ const Footer = () => {
 		<>
 			<hr className="mt-5" />
 			<div className="gap-y-5 w-full py-10 flex flex-col justify-center items-center md:flex-row md:justify-around md:py-20">
-				<div className=" flex items-center">
-					<Image
-						src="/handshake.png"
-						alt="favicono"
-						width={48}
-						height={48}
-						className="mr-2"
-					/>
-					<h1 className="font-semibold">Intern assistant</h1>
+				<div className="">
+					<div className="flex items-center">
+						<Image
+							src="/handshake.png"
+							alt="favicono"
+							width={48}
+							height={48}
+							className="mr-2"
+						/>
+						<h1 className="font-semibold">Intern assistant</h1>
+					</div>
+					<div className="text-gray-500">
+						<small>
+							icon by{" "}
+							<Link target="_blank" href="https://icons8.com">
+								Icons8
+							</Link>
+						</small>
+					</div>
+					<div className="text-gray-500">
+						<small>2023 copyright @Computer Science RMUTL and AIAT</small>
+					</div>
 				</div>
 
 				<div className="">
 					<h1 className="font-semibold uppercase">Services</h1>
-					<div className="flex gap-x-4">
+					<div className="flex gap-x-4 mt-3">
 						<Link
 							href="https://iamonze.tech/docs"
 							target="_blank"
@@ -27,8 +40,8 @@ const Footer = () => {
 						>
 							<p>API</p>
 						</Link>
-						<Link href="#" target="_blank" className="hover:text-purple-500">
-							<p>Document</p>
+						<Link href="/about" className="hover:text-purple-500">
+							<p>About</p>
 						</Link>
 						<Link href="#" target="_blank" className="hover:text-purple-500">
 							<p>Article</p>
